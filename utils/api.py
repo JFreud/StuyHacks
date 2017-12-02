7c5126a8537197d177cf0c5ee545c0e2940f4fbe
Sasha Fomina <afomina@stuy.edu>
2017-12-02 13:48:07 -0500

    Watson stuff

--- utils/api.py
+++ utils/api.py
@@ -6,6 +6,77 @@ TWITTER_CONSUMER_SECRET = "hE1w9Ott1NWjb8OIXlpbhRcyvO54TJt7sIJ8mPYeVXsKvRI2O7"
 TWITTER_ACCESS_TOKEN = "1426838582-YkU3XAVMUGXuUAazLdsIFyF5oFE4wpBcbkcq7o3"
 TWITTER_ACCESS_SECRET = "oecREA8AiMacla9ROxDKodPxKqy5D4vs2bEmfXWTrIE1G"
 
+IBM_USER = "ba0b75ff-9d1a-46b4-abfb-cb44113a2449"
+IBM_PASS= "CdnsqCmfLbns"
+
+def pretty(d, indent=0):
+   for key, value in d.items():
+      print('\t' * indent + str(key))
+      if isinstance(value, dict):
+         pretty(value, indent+1)
+      elif isinstance(d[key],list):
+         print "["
+         for item in d[key]:
+            pretty(item, indent+2)
+         print "]"
+      else:
+         print('\t' * (indent+1) + str(value))
+
+
+#--------------------WATSON ----------------------------------
+def run_watson(tweet):
+    
+    encoded_tweet = urllib2.quote(tweet)
+    
+    link =  'https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2017-02-27&text=' + encoded_tweet + "&features=keywords"
+
+    results = requests.get(link, auth=HTTPBasicAuth(IBM_USER,IBM_PASS))
+
+    #results is now a dictionary 
+    results = results.json()
+
+    print pretty(results)
+    return results 
+
+
+def filters_keys(keyword_dict):
+    keyword_dict = keyword_dict["keywords"]
+    keywords = []
+    keywords.append(keyword_dict[0]["text"])
+    
+    for item in keyword_dict :
+        #print "DDDDD: " + item["text"]
+        if len(keywords) ==3:
+            return keywords
+        
+        counter = 0
+        repeated = False
+        
+        for word in keywords:
+            word_list1 = word.split(" ")
+            #print word_list1
+            for i in word_list1:
+               if i in item["text"]:
+                  repeated = True
+            #print repeated 
+                  
+            
+            if item["text"] in word or repeated:
+                #print "SDFFFFFFFFFFFFF" 
+                break
+            else:
+                counter +=1
+                
+        if counter == len(keywords):
+            #print item["text"] 
+            keywords.append(item["text"])
+
+    stringed_keywords = (" ").join(keywords)
+   
+#------------------------------------------------------------
+
+#------------------------------TWITTER----------------------------
+
 def oauth_twitter(url, key, secret, http_method="GET", post_body="", http_headers=None):
     consumer = oauth2.Consumer(key=TWITTER_CONSUMER_KEY, secret=TWITTER_CONSUMER_SECRET)
     token = oauth2.Token(key=TWITTER_ACCESS_TOKEN, secret=TWITTER_ACCESS_SECRET)
@@ -41,7 +112,9 @@ def get_trump_texts():
             pass
     return text_list
 
+#------------------------------------------------------------
 
+#------------------------------NYT------------------------------
 
 #returns dict keyed by article headline and value is a list of [url, snippet, date]
 def get_articles():
@@ -59,7 +132,7 @@ def get_articles():
         articleInfo.append(article["pub_date"])
         article_list[article["headline"]["main"]] = articleInfo
     return article_list
-
+#-----------------------------------------------------------------
 
 if __name__ == "__main__":
     trump_urls = get_trump_urls()
